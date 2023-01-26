e12493adab7d336311ea8783e165e3cf2eefe064
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-01-26 09:54:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install nano python3 python3-pip -y
+pip3 install pyserial
 wget https://raw.githubusercontent.com/VitRuzicka/homescripts/main/otevirani.py
 chmod +x otevirani.py
 sudo mv otevirani.py /home/$USER/Desktop/
