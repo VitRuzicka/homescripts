9bde8288809ebd6c08b7f5351ed52d936dc50ffc
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-01-26 11:08:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install nano python3 python3-pip -y
+sudo apt install nano python3 python3-pip cron -y
 pip3 install pyserial
 wget https://raw.githubusercontent.com/VitRuzicka/homescripts/main/otevirani.py
 chmod +x otevirani.py
-sudo mv otevirani.py /home/$USER/Desktop/
+wget https://raw.githubusercontent.com/VitRuzicka/homescripts/main/debug.py
+chmod +x debug.py
+wget https://raw.githubusercontent.com/VitRuzicka/homescripts/main/start.sh
+chmod +x start.sh
+wget https://raw.githubusercontent.com/VitRuzicka/homescripts/main/monitor.desktop
+chmod +x monitor.desktop
+sudo mv monitor.desktop /home/linaro/Desktop/
