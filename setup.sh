3a3bf5bdef2d517a92d4107c276f29132adcaff8
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-01-26 09:50:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install nano -y
+wget https://raw.githubusercontent.com/VitRuzicka/homescripts/main/otevirani.py
+chmod +x otevirani.py
+sudo mv otevirani.py /home/$USER/Desktop/
