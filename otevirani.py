4bcb140f50c12f59da623c4581e31b4c89ce484b
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-01-26 10:01:41 +0100

    Update otevirani.py

--- otevirani.py
+++ otevirani.py
@@ -1,3 +1,11 @@
 import serial
-with serial.Serial('/dev/ttyS1', 115200, timeout=1) as ser:
+try:
+  ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+except FileNotFoundError:
+  quit()
+while True:
+  if input() == 'pomoc':
+    ser.write('o')
+    ser.write('z')
+ser.close()
   
