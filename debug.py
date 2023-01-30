b4975fd08955a2bad96db80fe902c8aacdd3d11b
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-01-30 09:50:30 +0100

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,9 +1,8 @@
 import serial
-import time
 try:
   ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-except FileNotFoundError:
-  quit()
-while True:
-  print(ser.readline())
-ser.close()
+  while 1:
+    print(ser.readline()[:-2])
+except ValueError:
+  print("seriovy port nenalezen, restartuj skript")
+  ser.close()
