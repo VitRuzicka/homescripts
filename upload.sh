a694d9bb95d86067949cee4ca81f93a340c6d072
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-02-02 16:22:20 +0100

    Add files via upload

---
+++ upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if test -f "rp2040load" ; then 
+    echo "rp2040load stažen"
+else
+    wget https://github.com/VitRuzicka/homescripts/raw/main/tools_linux_amd64/rp2040load
+    chmod +x rp2040load
+fi
+if test -f "picotool" ; then 
+    echo "picotool stažen"
+else
+    wget https://github.com/VitRuzicka/homescripts/raw/main/tools_linux_amd64/picotool
+    chmod +x picotool
+fi
+wget https://github.com/VitRuzicka/homescripts/raw/main/fw/firmware.elf #stazeni firmwaru
+uzivatel="${USER}"
+sudo stty -F /dev/ttyACM0 1200
+echo waiting
+echo nahravam program do rpi pico
+while [ ! -d /media/$uzivatel/RPI-RP2 ]; do sleep 0.1; done
+sleep 0.5
+#if [ "$*" = "" ]; then echo rebooting; sudo ./picotool reboot; exit; fi
+echo copying
+#cp $1 /media/$uzivatel/RPI-RP2
+./rp2040load -D firmware.elf -v
+echo done
+rm firmware*
