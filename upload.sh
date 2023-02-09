7f7b3047421c2edb53d95b1f8188da3170f092ae
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-02-09 09:18:44 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -12,6 +12,7 @@ else
     chmod +x picotool
 fi
 wget https://github.com/VitRuzicka/homescripts/raw/main/fw/firmware.elf #stazeni firmwaru
+wget https://github.com/VitRuzicka/homescripts/raw/main/fw/firmware.uf2
 uzivatel="${USER}"
 sudo stty -F /dev/ttyACM0 1200
 echo waiting
@@ -20,7 +21,7 @@ while [ ! -d /media/$uzivatel/RPI-RP2 ]; do sleep 0.1; done
 sleep 0.5
 #if [ "$*" = "" ]; then echo rebooting; sudo ./picotool reboot; exit; fi
 echo copying
-#cp $1 /media/$uzivatel/RPI-RP2
-./rp2040load -D firmware.elf -v
+cp $1 /media/$uzivatel/RPI-RP2
+#./rp2040load -D firmware.elf -v
 echo done
 rm firmware*
