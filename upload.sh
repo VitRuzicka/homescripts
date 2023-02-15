47b1461823df90c3739ffe09bd9e04774a8c74eb
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-02-15 10:12:07 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,17 +2,17 @@
 if test -f "rp2040load" ; then 
     echo "rp2040load stažen"
 else
-    wget https://github.com/VitRuzicka/homescripts/raw/main/rp2040load
+    wget --no-check-certificate https://github.com/VitRuzicka/homescripts/raw/main/rp2040load
     chmod +x rp2040load
 fi
 if test -f "picotool" ; then 
     echo "picotool stažen"
 else
-    wget https://github.com/VitRuzicka/homescripts/raw/main/picotool
+    wget --no-check-certificate https://github.com/VitRuzicka/homescripts/raw/main/picotool
     chmod +x picotool
 fi
-wget https://github.com/VitRuzicka/homescripts/raw/main/fw/firmware.elf #stazeni firmwaru
-wget https://github.com/VitRuzicka/homescripts/raw/main/fw/firmware.uf2
+wget --no-check-certificate https://github.com/VitRuzicka/homescripts/raw/main/fw/firmware.elf #stazeni firmwaru
+wget --no-check-certificate https://github.com/VitRuzicka/homescripts/raw/main/fw/firmware.uf2
 uzivatel="${USER}"
 sudo stty -F /dev/ttyACM0 1200
 echo waiting
