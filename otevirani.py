71ffe97f658eeb47173e623ca4824325cdc5cfcd
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-03-06 21:23:43 +0100

    Update otevirani.py

--- otevirani.py
+++ otevirani.py
@@ -1,15 +1,30 @@
-#!/usr/bin/python3
-import serial
+#!/usr/bin/env python
+import minimalmodbus
+from datetime import datetime
+import os
+import threading
+from threading import Thread
 import time
-from serial import SerialException
-key = "pomoc"
-while 1:
-        if(input() == key):
-                try:
-                        with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as ser:
-                                ser.write(b'o')
-                                time.sleep(1)
-                                ser.write(b'z')
-                                ser.close()
-                except SerialException:
-                        print("home deska neni pripojena")
+
+now = datetime.now()
+instrument = 0
+def pripoj():
+    global instrument
+    instrument = minimalmodbus.Instrument("/dev/ttyACM0", 3) # port name, slave address (in decimal)
+def posli(p):
+    instrument.write_register(27, p, 0)
+def loguj(cas):
+    while True:
+        file1 = open("log.txt", "a")
+        str1 = str(now.strftime("%d/%m/%Y, %H:%M:%S, ")+str(instrument.read_register(22,0))+" "+str(instrument.read_register(31, 0)) + " \n")
+        file1.write((str1))
+        file1.close()
+        time.sleep(cas*60)
+if __name__ == '__main__':
+    pripoj()
+    daemon = Thread(target=loguj, args=(5,), daemon=True, name='Logovani')
+    daemon.start()
+    while True:
+        r = input()
+        if r == "pomoc":
+            posli(1)
