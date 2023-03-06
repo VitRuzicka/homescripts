21ffb4b0a0564d2fb73e0f786fe2fd87976d7953
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2023-03-06 21:22:55 +0100

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,8 +1,82 @@
-import serial
-try:
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-    while 1:
-      print(ser.readline()[:-2])
-except ValueError:
-    print("seriovy port nenalezen, restartuj skript")
-    ser.close()
+#!/usr/bin/env python
+import minimalmodbus
+import serial.tools.list_ports
+import time
+import os
+instrument = 0
+
+def najdiPorty():
+    ports = serial.tools.list_ports.comports()
+    if ports == []:
+        print("Nenalezen volný port")
+        print("Končím")
+        quit()
+    i = 0
+    for port, desc, hwid in sorted(ports):
+        i+=1
+        print("[{}] {}: {}".format(i, port, desc))
+        
+    volba = input("Zadejte cislo portu: ")
+    serport = str(sorted(ports)[int(volba)-1])
+    serport = serport[:12:]
+    print("vybran ", serport)
+    return serport
+
+def pripoj(zvolenyPort):
+    global instrument
+    instrument = minimalmodbus.Instrument(zvolenyPort, 3) # port name, slave address (in decimal)
+    print("pripojuji k %s"%(zvolenyPort))
+def vytiskniHodnoty(hodn):
+    os.system("clear")         
+    print("------------------------------")
+    print("reg|    name          |value  |")
+    for i, p in enumerate(sluzby):
+        print ("|%2d| %16s | %3d   |"%(i+10,p, hodn[i]))
+    print("------------------------------")
+    
+def ziskejHodnoty():
+    hodn = []
+    for i in range(len(sluzby)):
+        hodn.append(instrument.read_register(i+10, 0))
+    vytiskniHodnoty(hodn)
+
+def zmen(reg, hod):
+    instrument.write_register(int(reg), int(hod), 0) # Registernumber, value, number of decimals for storage
+
+sluzby = [
+        "RED led",
+        "GREEN led",
+        "BLUE led",
+        "Prolinani barev",
+        "Brightness",
+        "WHITE led",
+        "UVC led",
+        "IR brana dopis",
+        "IR brana balik",
+        "COOL tl.",
+        "NOUZE tl.",
+        "fotoodpor",
+        "Temp. fridge",
+        "OUT1",
+        "OUT2",
+        "IN2",
+        "IN3",
+        "ZAMEK",
+        "ZAMEK sense",
+        "COOL ZAMEK",
+        "COOL ZAMEK sense",
+        "Test in"
+]
+
+if __name__ == '__main__':
+    port = najdiPorty()
+    pripoj(port)
+    while True:
+        ziskejHodnoty()
+        t = input("[ENTER] pro refresh, [NUM] pro změnu registru: ")
+        if t != '' and 0 < int(t) and int(t) < 50:
+            zmen(t, int(input("Zadejte hodnotu registru: ")))
+        else:
+            time.sleep(1)
+
+
